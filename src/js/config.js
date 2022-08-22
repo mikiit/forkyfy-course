f788c3a3f85808141b70bf8f39f10c0d6b93a18b
mikiit <markovicm.miroslav@gmail.com>
2022-08-22 11:45:50 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '80347780-2f62-408d-8fa0-a4ca03602ee3';
+export const MODAL_CLOSE_SEC = 2.5;
